5b0c37898d7086f2c3848fcc6479a7dc0bc6a8d1
Paul Jaros <jaros.paul@gmail.com>
2024-12-11 17:50:28 +0100

    add --delete to deploy script

--- publish_rsync.sh
+++ publish_rsync.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -vr --progress /home/jarop/git/sg-linuxtreff-ch/output/ "sg.linuxtreff.ch:sg.linuxtreff.ch"
+rsync -vr --progress --delete /home/jarop/git/sg-linuxtreff-ch/output/ "sg.linuxtreff.ch:sg.linuxtreff.ch"
